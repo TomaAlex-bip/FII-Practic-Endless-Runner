5f842c028da61310fc9379b338b7d8f2452b5b5e
Toma Alexandru <toma.alex.xd@gmail.com>
2022-04-14 20:24:32 +0300

    bugfixes score points pool

--- Endless-Runner-Game/Assets/_Root Assets/Scripts/UI/MenuController.cs
+++ Endless-Runner-Game/Assets/_Root Assets/Scripts/UI/MenuController.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
     [SerializeField] private Transform infoPanel;
+    [SerializeField] private Text highScore;
+
+    private void Awake()
+    {
+        highScore.text = HighScoreSaveManager.GetHighScore().ToString();
+    }
 
     public void OpenInfoPanel() => infoPanel.gameObject.SetActive(true);
     public void CloseInfoPanel() => infoPanel.gameObject.SetActive(false);
 
     public void StartGame() => SceneManager.LoadScene(1);
     public void QuitGame() => Application.Quit();
+    
+    
 }
